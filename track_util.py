f92e59bbc47f680b7746289978cb7bc0b270f738
Justin Reiser <128163422+jwreiser@users.noreply.github.com>
2024-03-21 10:50:25 -0400

    working on returning valid content

--- track_util.py
+++ track_util.py
@@ -120,9 +120,9 @@ def get_songs_by_audio_attributes(sp, playlist_id):
             'summer','trance','trip-hop','work-out','world-music']
     genre=random.choice(genres)
     songs=[]
-    recommendations = sp.recommendations(seed_genres=[genre],min_acousticness=attributes['acousticness']-.05,max_acousticness=attributes['acousticness']+.05
-                                         ,min_danceability=attributes['danceability']-.05,max_danceability=attributes['danceability']+.05
-                                         ,min_energy=attributes['energy']-.05,max_energy=attributes['energy']+.05
+    recommendations = sp.recommendations(limit=100,seed_genres=[genre],min_acousticness=attributes['acousticness']-.075,max_acousticness=attributes['acousticness']+.075
+                                         ,min_danceability=attributes['danceability']-.075,max_danceability=attributes['danceability']+.075
+                                         ,min_energy=attributes['energy']-.075,max_energy=attributes['energy']+.075
                                          # , target_key=attributes['key'] causes ambiguous error
                                          # , target_tempo=attributes['tempo'] causes ambiguous error
                                          # , target_valence=attributes['valence'] causes ambiguous error
